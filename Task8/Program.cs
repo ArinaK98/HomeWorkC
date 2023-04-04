64d1716b29398109e61b3c979c0727ffb0093f89
KumalyakovaAS <explay1355@mail.ru>
2023-04-04 22:11:53 +0300

    ДЗ 8

---
+++ Task8/Program.cs
@@ -0,0 +1,97 @@
+﻿// Задача 54
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             matrix[i, j] = new Random().Next(1, 11); // [-10, 10]
+//     }
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             Console.Write($"{matrix[i, j]} \t");
+//         Console.WriteLine();
+//     }
+// }
+
+// void ReplaceNum(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             // int minPosition = matrix[0, j] ;
+//             for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+//             {
+//                 if (matrix[i, k] < matrix[i, k + 1]) 
+//                 {      
+//                     int temp = matrix[i, k + 1];
+//                     matrix[i, k + 1] = matrix[i, k];
+//                     matrix[i, k] = temp;
+//                 }
+//             }
+//         }                          
+//     }
+// }
+
+// Console.Clear();
+// Console.Write("Введите размеры двумерного массива: ");
+// int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+// PrintMatrix(matrix);
+// Console.WriteLine();
+// ReplaceNum(matrix);
+// PrintMatrix(matrix);
+
+// Задача 56
+// void InputMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             matrix[i, j] = new Random().Next(0, 10); // [0, 9]
+// }
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrix[i, j]} \t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int StrMinSum(int[,] matrix)
+// {
+//     int min, count=0; 
+//     int[] sum = new int[matrix.GetLength(0)];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//             sum[i] += matrix[i, j];   
+//     min = sum[0];
+//     for (int i = 1; i < sum.Length; i++)
+//         if (sum[i] < min)
+//             {
+//                 min = sum[i]; 
+//                 count=i;
+//             }
+//     return count;
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите размерность массива: ");
+// int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+// PrintMatrix(matrix);
+// Console.WriteLine($"Строка с наименьшей суммой элементов: {StrMinSum(matrix)}");
+
+
